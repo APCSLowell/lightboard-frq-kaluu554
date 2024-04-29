d5dcbc85e4c31853e4c17dd3b2ae02d31d23bc54
kaluu554 <143027760+kaluu554@users.noreply.github.com>
2024-04-29 02:01:42 -0700

    Update LightBoard.java

--- src/main/java/LightBoard.java
+++ src/main/java/LightBoard.java
@@ -15,7 +15,7 @@ public class LightBoard
     
     for(int r = 0; r< numRows; r++){
       for(int c = 0; c<numCols; c++){
-        if(Math.random() <= 0.4){
+        if(Math.random() < 0.4){
           lights[r][c] = true;
         } else{
           lights[r][c] = false;
